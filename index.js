488ea43ec798c573ad20e435a82b888e7dd09ad2
Juan Carlos Medina <juanca@mavenlink.com>
2017-11-21 15:24:39 -0800

    css modules init

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ import styles from './styles.css';
 console.log('Sanity check.');
 
 const div = window.document.createElement('div');
-div.className = 'container'; // styles.container;
+div.className = styles.container;
 
 window.document.body.appendChild(div);
--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
         test: /\.css$/,
         use: [
           'style-loader',
-          'css-loader' ,
+          { loader: 'css-loader', options: { modules: true } },
         ],
       },
     ],
