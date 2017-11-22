c6ba978e986061383ac8aafe6676f4d30f1d234e
Juan Carlos Medina <juanca@mavenlink.com>
2017-11-21 16:31:01 -0800

    Implement basic sass import example

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ import styles from './styles.css';
 console.log('Sanity check.');
 
 const div = window.document.createElement('div');
-div.className = styles.container;
+div.className = `${styles.container} global-classname`;
 
 window.document.body.appendChild(div);
